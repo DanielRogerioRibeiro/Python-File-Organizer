f140bb10a7080072c08b3dd9dee71c3651b3e33b
Daniel Rogerio Ribeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-11 00:48:38 -0300

    Inclusion of copy, move functions. Creation of Labels and Copy, Move and Delete buttons.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Organizador de Arquivo
 
 import os
+import shutil
 
 from tkinter import*
 from tkinter import Tk, StringVar, ttk
@@ -8,6 +9,16 @@ from tkinter import filedialog as fd
 from tkinter import messagebox
 
 from PIL import Image, ImageTk
+
+#Importando a parte Gráfica
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+
+
+#Importando a Aba View.py
+from view import *
 
 ################# colors ###############
 
@@ -28,25 +39,33 @@ colors = ['#1f77b4', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555','#1f77
 
 ################# creating window ###############
 
-window = Tk ()
-window.title ("")
-window.geometry('700x350')
-window.configure(background=co9)
-window.resizable(width=FALSE, height=FALSE)
+janela = Tk ()
+janela.title ("")
+janela.geometry('700x350')
+janela.configure(background=co9)
+janela.resizable(width=FALSE, height=FALSE)
 
-# Definindo o Estilo da Janela
-style = ttk.Style(window)
+style = ttk.Style(janela)
 style.theme_use("clam")
-
 
 ################# Creating Frames ####################
 
-frameUp = Frame(window, width=700, height=50, bg=co1,  relief="flat",)
+frameUp = Frame(janela, width=700, height=50, bg=co1,  relief="flat",)
 frameUp.grid(row=0, column=0)
 
-frameMiddle = Frame(window,width=700, height=300,bg=co1)
+frameMiddle = Frame(janela,width=700, height=300,bg=co1)
 frameMiddle.grid(row=1, column=0,sticky=NSEW)
 
+
+# ------------- Creating individual frames -------------------------
+
+frame_operations = Frame(frameMiddle, width=300, height=350,bg=co1)
+frame_operations.grid(row=0,column=0)
+
+frame_graph = Frame(frameMiddle, width=400, height=350,bg=co1)
+frame_graph.place(x=170,y=0)
+
+frame_operations.lift()
 
 # opening image
 
@@ -54,26 +73,169 @@ app_img = Image.open('imagens/icon.png')
 app_img = app_img.resize((45, 45))
 app_img = ImageTk.PhotoImage(app_img)
 
-app_logo = Label(frameUp, image=app_img, text="File Organizer App", width=700, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'),bg= co1, fg=co4 )
+app_logo = Label(frameUp, image=app_img, text="File organizer App", width=700, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'),bg= co1, fg=co4 )
 app_logo.place(x=0, y=0)
+ 
 
-#Trabalhando no Frame meio
+# pie chart function ------------------------
+def chart_pie(list_1, list_2):
+     # make figure and assign axis objects
+     figure = plt.Figure(figsize=(5, 3), dpi=100)
+     ax = figure.add_subplot(111)
 
-#Dividindo o Frame meio em Operações (lado Esquerdo) e Gráfico (Lado Direito)
+     list_values = list_1
+     extension_list = list_2
 
-frame_operations = Frame(frameMiddle, width=300, height=350,bg=co1)
-frame_operations.grid(row=0,column=0)
+     ax.pie(list_values, wedgeprops=dict(width=0.3), colors=colors,shadow=True, startangle=90)
 
-frame_graphic = Frame(frameMiddle, width=400, height=350,bg=co1)
-frame_graphic.place(x=170,y=0)
+     ax.legend(extension_list, loc="center right", bbox_to_anchor=(1.55, 0.50))
 
-frame_operations.lift()
-
-#criando botões e entradas
+     canva = FigureCanvasTkAgg(figure, frame_graph)
+     canva.get_tk_widget().grid(row=0,column=0,sticky=NSEW)
 
 # Select Folder -----------------------------------
 
 l_app = Label(frame_operations, text="Select Folder", anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_app.place(x=10, y=10)
 
-window.mainloop()
+
+# defining global the folder variable
+global folder
+
+
+# function to choose the folder
+def choose_folder():
+     global folder
+    
+     folder = fd.askdirectory()
+
+     # creating the list of extensions
+     extension_list = ['.txt','.csv','.xlsx','.pdf','.zip','.jpg','.png','.mp3','.mp4','. py','.html','.css','.js']
+     combobox_extension_list = []
+     list_extension_counted = []
+     list_extension_values = []
+
+     number_of_folders = 0
+
+     for i in extension_list:
+         folder_path = folder
+         extension = i
+         num_files, num_folders = count_files_and_folders(folder_path, extension)
+
+         if num_files<=0:
+             pass
+         else:
+             list_extension_counted.append(i + ": " + str(num_files))
+             combobox_extension_list.append(i)
+             list_extension_values.append(num_files)
+             number_of_folders = num_folders
+
+
+     # added the total number of folders
+     list_extension_counted.append('folders: ' + str(number_of_folders))
+     list_extension_values.append(number_of_folders)
+
+     # passing extensions to the combobox
+     combo_extension ['values'] = (combobox_extension_list)
+     combo_extension_move ['values'] = (combobox_extension_list)
+
+     # sending the values to the graph by calling the graph function
+     chart_pie(list_extension_values, list_extension_counted)
+
+
+b_choose = Button(frame_operations,command=choose_folder, text="Choose ",width=15, overrelief=RIDGE, font=('ivy 8'),bg=co1, fg=co4 )
+b_choose.place(x=10, y=40)
+
+
+# Copying Files -----------------------------------
+
+l_app = Label(frame_operations, text="Copy Files", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_app.place(x=10, y=91)
+
+
+def copy():
+     global folder
+
+     # getting extension
+     extension = combo_extension.get()
+
+     print(folder)
+
+     # picking up destination
+     destination = fd.askdirectory()
+
+     # calling function copy files
+     copy_files(folder,destination,extension)
+
+     # inform
+     messagebox.showinfo('Copy', "The files were successfully copied")
+
+     # clear field
+     combo_extension.delete(0,END)
+
+    
+combo_extension = ttk.Combobox(frame_operations, width=5,font=('Ivy 10'))
+combo_extension.place(x=110, y=91)
+
+# copy button
+img_copy = Image.open('imagens/copiar.png')
+img_copy = img_copy.resize((17,17))
+img_copy = ImageTk.PhotoImage(img_copy)
+
+b_copy = Button(frame_operations,command=copy,image=img_copy, compound=LEFT, anchor=NW, text="Choose Destination ",width=110, overrelief=RIDGE, font=('ivy 8'),bg=co1, fg=co4)
+b_copy.place(x=171, y=90)
+
+
+# Move Files -----------------------------------
+
+l_app = Label(frame_operations, text="Move Files", height=1,anchor=NW,relief="flat", font=('Ivy 10'), bg=co1, fg=co4)
+l_app.place(x=10, y=141)
+
+def move():
+     global folder
+
+     # getting extension
+     extension = combo_extension_move.get()
+
+     # picking up destination
+     destination = fd.askdirectory()
+
+     source_folder = folder
+     destination_folder = destination
+     file_extension = extension
+
+     # calling the function to move the files
+     move_files_by_extension(source_folder, destination_folder, file_extension)
+
+     # inform
+     messagebox.showinfo('Move', "The files were successfully moved")
+
+     # clear field
+     combo_extension_move.delete(0,END)
+
+combo_extension_move = ttk.Combobox(frame_operations, width=5,font=('Ivy 10'))
+combo_extension_move.place(x=110, y=141)
+
+# move button
+img_move = Image.open('imagens/mover.png')
+img_move = img_move.resize((17,17))
+img_move = ImageTk.PhotoImage(img_move)
+
+b_move = Button(frame_operations,command=move,image=img_move, compound=LEFT, anchor=NW, text="Choose destination ",width=110, overrelief=RIDGE, font=('ivy 8'),bg=co1, fg=co4)
+b_move.place(x=171, y=140)
+
+# Excluir Arquivos -----------------------------------
+
+l_app = Label(frame_operations, text="Delete Files", anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_app.place(x=10, y=191)
+
+
+# Delete button
+img_delete = Image.open('imagens/delete.png')
+img_delete = img_delete.resize((17,17))
+img_delete = ImageTk.PhotoImage(img_delete)
+
+b_mover = Button(frame_operations,image=img_delete, compound=LEFT, anchor=NW, text="Delete files ",width=110, overrelief=RIDGE, font=('ivy 8'),bg=co1, fg=co4 )
+b_mover.place(x=171, y=190)
+
+janela.mainloop ()
--- view.py
+++ view.py
@@ -1,19 +1,55 @@
 import os
 import shutil
 
-# codigo para contar o numero de arquivos e pastas
+# view codes ---------------------------
+
+import os
+import shutil
+
+# code to count files and number of folders
 def count_files_and_folders(folder_path, extension):
-    num_files = 0
-    num_folders = 0
+     num_files = 0
+     num_folders = 0
 
-    for item in os.listdir(folder_path):
-        if os.path.isfile(os.path.join(folder_path, item)) and item.endswith(extension):
-            num_files += 1
-        elif os.path.isdir(os.path.join(folder_path, item)):
-            num_folders += 1
-            sub_folder_path = os.path.join(folder_path, item)
-            sub_num_files, sub_num_folders = count_files_and_folders(sub_folder_path, extension)
-            num_files += sub_num_files
-            num_folders += sub_num_folders
+     for item in os.listdir(folder_path):
+         if os.path.isfile(os.path.join(folder_path, item)) and item.endswith(extension):
+             num_files += 1
+         elif os.path.isdir(os.path.join(folder_path, item)):
+             num_folders += 1
+             sub_folder_path = os.path.join(folder_path, item)
+             sub_num_files, sub_num_folders = count_files_and_folders(sub_folder_path, extension)
+             num_files += sub_num_files
+             num_folders += sub_num_folders
 
-    return num_files, num_folders
+     return num_files, num_folders
+
+
+# code to copy files
+def copy_files(source,destination,ext):
+
+     source_directory = source
+     destination_directory = destination
+
+     extension = ext
+
+     for file_name in os.listdir(source_directory):
+         if file_name.endswith(extension):
+             source_path = os.path.join(source_directory, file_name)
+             destination_path = os.path.join(destination_directory, file_name)
+             shutil.copy(source_path, destination_path)
+
+
+
+# Code to move files
+
+def move_files_by_extension(source_folder, destination_folder, file_extension):
+     if not os.path.exists(destination_folder):
+         os.makedirs(destination_folder)
+
+     for file_name in os.listdir(source_folder):
+         if file_name.endswith(file_extension):
+             file_path = os.path.join(source_folder, file_name)
+             shutil.move(file_path, destination_folder)
+
+
+# view codes End ---------------------------

9324f06e03f98f775f8ff192e3a3b192573a844f
Daniel Rogerio Ribeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-11 21:43:28 -0300

    Added Copy, Paste, and Delete buttons. Defined the functions for Copy, Paste and Delete.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tkinter import*
 from tkinter import Tk, StringVar, ttk
 from tkinter import filedialog as fd
 from tkinter import messagebox
+import customtkinter
 
 from PIL import Image, ImageTk
 
@@ -36,19 +37,21 @@ co9 = "#e9edf5"  # + green
 colors = ['#1f77b4', '#66bbbb','#99bb55', '#ee9944', '#444466', '#bb5555','#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2' ,'#7f7f7f','#bcbd22','#17becf',]
 ['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2' ,'#7f7f7f','#bcbd22','#17becf','#ff9896','#98df8a','#aec7e8','#ffbb78' ,'#c5b0d5','#c49c94','#f7b6d2','#dbdb8d','#9edae5','#393b79']
 
+customtkinter.set_appearance_mode("dark")
 
-################# creating window ###############
+################# Criando Janela ###############
 
-janela = Tk ()
-janela.title ("")
+janela = customtkinter.CTk()
+janela.title ("File Organizer")
 janela.geometry('700x350')
 janela.configure(background=co9)
 janela.resizable(width=FALSE, height=FALSE)
 
-style = ttk.Style(janela)
-style.theme_use("clam")
 
-################# Creating Frames ####################
+#style = ttk.Style(janela)
+#style.theme_use("clam")
+
+################# Criando Frames ####################
 
 frameUp = Frame(janela, width=700, height=50, bg=co1,  relief="flat",)
 frameUp.grid(row=0, column=0)
@@ -57,7 +60,7 @@ frameMiddle = Frame(janela,width=700, height=300,bg=co1)
 frameMiddle.grid(row=1, column=0,sticky=NSEW)
 
 
-# ------------- Creating individual frames -------------------------
+# ------------- Criando Frame Individual -------------------------
 
 frame_operations = Frame(frameMiddle, width=300, height=350,bg=co1)
 frame_operations.grid(row=0,column=0)
@@ -93,17 +96,17 @@ def chart_pie(list_1, list_2):
      canva = FigureCanvasTkAgg(figure, frame_graph)
      canva.get_tk_widget().grid(row=0,column=0,sticky=NSEW)
 
-# Select Folder -----------------------------------
+# Selecionando Pasta -----------------------------------
 
 l_app = Label(frame_operations, text="Select Folder", anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_app.place(x=10, y=10)
 
 
-# defining global the folder variable
+# Definindo variável global
 global folder
 
 
-# function to choose the folder
+# função para escolher pasta
 def choose_folder():
      global folder
     
